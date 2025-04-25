cf39f2e1db34400ea19796a07282ab20beba4dad
Devendra12vi <devendravishwakrma44@gmail.com>
2025-04-25 10:57:41 +0530

    Add files via upload

---
+++ in.js
@@ -0,0 +1,13 @@
+
+function confirmAction() {
+   let result= confirm("Do you want to continue?");
+   alert((result ? "you are selected" : "you are not selected"));
+}
+function confirmh(){
+    let result = confirm("Are you Himanshu?");
+    alert(result ? "HII Chutiye" : "Hello dear Friends";
+}
+function confirmAnotherAction() {
+    let result = confirm("Do you want to perform another action?");
+    alert(result ? "Action confirmed!" : alert("Action canceled!"));
+}
---
+++ main.js
@@ -0,0 +1,63 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});//   
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+
+
+const progressBar = document.querySelectorAll(".about__progressbar");
+
+Array.from(progressBar).forEach((bar) => {
+  const progress = bar.dataset.progress;
+  bar.querySelector("span").style.width = progress + "%";
+});
+
+
+const tabList = document.querySelector(".resume__tablist");
+
+tabList.addEventListener("click", (e) => {
+  const tabIndex = e.target.dataset.tab;
+  if (!tabIndex) return;
+
+  const tabs = document.querySelectorAll("[data-tab]");
+  Array.from(tabs).forEach((tab) => {
+    if (tab.dataset.tab === tabIndex) {
+      tab.classList.add("active");
+    } else {
+      tab.classList.remove("active");
+    }
+  });
+
+  const activePanel = document.querySelector(".panel__grid.active");
+  const toActivePanel = document.querySelector(`[data-panel="${tabIndex}"]`);
+  if (activePanel.dataset.panel === tabIndex) return;
+  activePanel.classList.add("close");
+  activePanel.addEventListener(
+    "animationend",
+    (e) => {
+      activePanel.classList.remove("active");
+      activePanel.classList.remove("close");
+      toActivePanel.classList.add("active");
+    },
+    { once: true }
+  );
+});
+
